cea735e053904bd54b29a704cf17c43ade2a1a30
Hector de Isidro <hector.isidro@mobusi.com>
2015-04-08 13:41:24 +0200

    Demo reworked

--- horizontalnumberpicker-demo/src/main/java/com/hules/horizontalnumberpicker/demo/MainActivity.java
+++ horizontalnumberpicker-demo/src/main/java/com/hules/horizontalnumberpicker/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.hules.horizontalnumberpicker.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import com.hrules.horizontalnumberpicker.HorizontalNumberPicker;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -10,5 +11,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HorizontalNumberPicker horizontalNumberPicker2 = (HorizontalNumberPicker) findViewById(R.id.horizontal_number_picker2);
+        HorizontalNumberPicker horizontalNumberPicker3 = (HorizontalNumberPicker) findViewById(R.id.horizontal_number_picker3);
+
+        horizontalNumberPicker2.getButtonMinusView().setText("<");
+        horizontalNumberPicker2.getButtonPlusView().setText(">");
+        horizontalNumberPicker2.setShowLeadingZeros(true);
+        horizontalNumberPicker2.setValue(23);
+
+        horizontalNumberPicker3.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+        horizontalNumberPicker3.getTextValueView().setTextColor(getResources().getColor(android.R.color.holo_red_light));
+        horizontalNumberPicker3.getTextValueView().setTextSize(22);
+
     }
 }
